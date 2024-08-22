878ce8a68217e541eae4234a12147a15952a8486
Luis Victoria <v@lambda.lv>
2024-08-22 05:36:53 +0000

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix> ];
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  programs.git = {
+    enable = true;
+    config = {
+      user = {
+        name = "Luis Victoria";
+        email = "v@lambda.lv";
+      };
+    };
+  };
+}
