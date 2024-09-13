b971b65aac5a30ef90905c18ae1f64fddc10c545
Luis Victoria <v@lambda.lv>
2024-09-13 18:13:37 -0400

    Import `home-manager` into `configuration.nix`

--- configuration.nix
+++ configuration.nix
@@ -2,14 +2,14 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       ./modules/main-user.nix
-      # <home-manager/nixos>
+      inputs.home-manager.nixosModules.default
     ];
 
   # Enable Nix Flakes
