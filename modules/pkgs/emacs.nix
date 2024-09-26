5e7acd445b6e7bffe99d3c87330cb82e9047ed99
Luis Victoria <v@lambda.lv>
2024-09-26 00:13:32 -0400

    Build Emacs with necessary dependencies

--- modules/pkgs/emacs.nix
+++ modules/pkgs/emacs.nix
@@ -3,33 +3,44 @@
 let
   secrets = import ../../secrets.nix;
   configDir = "${config.home.homeDirectory}/.config/emacs";
+
+  emacsWithDeps = pkgs.emacs.overrideAttrs (oldAttrs: rec {
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [
+      pkgs.sqlite
+      pkgs.libvterm
+      pkgs.libtool
+      pkgs.pkg-config
+      pkgs.gcc
+      pkgs.autoconf
+      pkgs.automake
+      pkgs.gnumake
+    ];
+  });
 in
 {
-  # Add emacs (and dependencies) to main user packages
+  # Use customized Emacs package via programs.emacs.package
+  programs.emacs = {
+    enable = true;
+    package = emacsWithDeps;
+  };
+
+  # Include other necessary packages
   home.packages = with pkgs; [
-    emacs
-
-    # dependencies
-    cmake
+    nil     # Nix LSP
+    pyright # Python LSP
     ripgrep
-
-
-    ## Latex
     texliveFull
-
-    ## VTerm
-    libtool
-    libvterm
-
-
-    ## LSPs
-    ### Nix
-    nil
-
-    ### Python
-    pyright
   ];
 
+  # Set environment variables to help Emacs find headers
+  home.sessionVariables = {
+    # Add the include path for sqlite
+    CPATH = "${pkgs.sqlite}/include";
+
+    # Add the library path for sqlite
+    LIBRARY_PATH = "${pkgs.sqlite}/lib";
+  };
+  
   # Home Manager configuration for Emacs and dotfiles
   home.activation = {
     cloneDotfiles = inputs.home-manager.lib.hm.dag.entryBefore ["writeBoundary"] ''
