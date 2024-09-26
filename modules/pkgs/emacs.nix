ee314cb3dd5256d9885a48d2af96cabb3b9fa974
Luis Victoria <v@lambda.lv>
2024-09-26 01:31:53 -0400

    Add compiler flags to Emacs build (doesn't fix sqlite build)

--- modules/pkgs/emacs.nix
+++ modules/pkgs/emacs.nix
@@ -4,16 +4,39 @@ let
   secrets = import ../../secrets.nix;
   configDir = "${config.home.homeDirectory}/.config/emacs";
 
-  emacsWithDeps = pkgs.emacs.overrideAttrs (oldAttrs: rec {
+  emacsWithCustomOptions = pkgs.emacs.overrideAttrs (oldAttrs: rec {
+    configureFlags = (oldAttrs.configureFlags or []) ++ [
+      # "--enable-profiling" # Use low-level profiling via gprof; reduces perfomance and replaces Emacs' profiler.el
+      "--enable-link-time-optimization" # Compiler optimization performed on per-file basis, better performance at cost of greater build time
+      "--with-cairo" # Better rendering performance
+      "--with-dbus" # Enable desktop communication with Emacs (e.g. notifications and desktop integration
+      "--with-file-notifications=inotify" # Use inotify for file change notifications
+      "--with-gnutls" # Enable support for secure connections over SSL/TLS
+      "--with-imagemagick" # Enable image support
+      "--with-json" # Enable native JSON support
+      "--with-mailutils" # Enable Mail
+      "--with-modules" # Enable use of external modules
+      "--with-native-compilation" # Native Lisp Compilation; significantly improves performance
+      "--with-pdumper" # Use over `unexexc` for faster startup and dumping
+      "--with-pgtk"  # GTK support for Wayland
+      # "--with-sound=alsa" # Using ALSA sound system
+      "--with-tree-sitter" # Enable syntax highlighting and parsing using tree-sitter; better performance editing code
+      "--with-x" # Use XWayland as fallback for packages or features not yet ported to Wayland
+      "--without-selinux" # Disable as NixOS by default does not use SELinux
+    ];
+
     nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [
-      pkgs.sqlite
-      pkgs.libvterm
-      pkgs.libtool
-      pkgs.pkg-config
-      pkgs.gcc
       pkgs.autoconf
       pkgs.automake
+      pkgs.gcc
       pkgs.gnumake
+      pkgs.gtk3
+      pkgs.imagemagick
+      pkgs.libgccjit
+      pkgs.libtool
+      pkgs.libvterm
+      pkgs.pkg-config
+      pkgs.sqlite
     ];
   });
 in
@@ -21,7 +44,7 @@ in
   # Use customized Emacs package via programs.emacs.package
   programs.emacs = {
     enable = true;
-    package = emacsWithDeps;
+    package = emacsWithCustomOptions;
   };
 
   # Include other necessary packages
@@ -34,11 +57,9 @@ in
 
   # Set environment variables to help Emacs find headers
   home.sessionVariables = {
-    # Add the include path for sqlite
-    CPATH = "${pkgs.sqlite}/include";
-
-    # Add the library path for sqlite
-    LIBRARY_PATH = "${pkgs.sqlite}/lib";
+    # Add the include and library paths for sqlite
+    EMACSQL_SQLITE3_INCLUDE_DIR = "${pkgs.sqlite}/include";
+    EMACSQL_SQLITE3_LIBRARY_DIR = "${pkgs.sqlite}/lib";
   };
   
   # Home Manager configuration for Emacs and dotfiles
