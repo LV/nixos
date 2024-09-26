2bd150ef41479e77a4078746b91012569638e6ae
Luis Victoria <v@lambda.lv>
2024-09-25 20:47:54 -0400

    Create `secrets.nix`

---
+++ secrets.nix
@@ -0,0 +1,6 @@
+# secrets.nix stores all sensitive variable names in my configuration; purposely empty variables for the sake of security
+{
+  # GitHub
+  githubUsername = "";
+  githubToken = "";
+}
