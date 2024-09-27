877b97034ce012bfb064ecc879287419471439e4
Luis Victoria <v@lambda.lv>
2024-09-27 03:47:05 -0400

    Add `symlink` from `secrets.nix` to `secrets.template.nix`

--- secrets.nix
+++ secrets.nix
@@ -1,10 +1,1 @@
-# secrets.nix stores all sensitive variable names in my configuration; purposely empty variables for the sake of security
-{
-  githubUsername = "";
-  githubToken = "";
-
-  wifiNetwork = {
-    ssid = "";
-    passkey = "";
-  };
-}
+secrets.template.nix
---
+++ secrets.template.nix
@@ -0,0 +1,10 @@
+# Fill in with appropriate values and rename this file to `secrets.nix`
+{
+  githubUsername = "";
+  githubToken = "";
+
+  wifiNetwork = {
+    ssid = "";
+    passkey = "";
+  };
+}
