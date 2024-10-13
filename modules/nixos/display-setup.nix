5978869983444695514787f7175a5f5f17f274ed
Luis Victoria <v@lambda.lv>
2024-10-13 15:49:17 -0400

    Remove non-working elements in `display-setup.nix`

--- modules/nixos/display-setup.nix
+++ modules/nixos/display-setup.nix
@@ -3,41 +3,10 @@
 {
   # Install swaylock and Hyprland
   environment.systemPackages = with pkgs; [
-    swaylock
     hyprland
-    waybar
   ];
 
   programs.hyprland = {
     enable = true;
-  };
-
-  # Define systemd service for the lock screen (swaylock)
-  systemd.user.services.swaylock = {
-    description = "Swaylock Lock Screen";
-    wantedBy = [ "graphical-session.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.swaylock}/bin/swaylock -c 000000"; # `-c` refers to color and 000000 is the color hex
-    };
-  };
-
-  # Define systemd service for Hyprland
-  systemd.user.services.hyprland = {
-    description = "Hyprland Session";
-    wantedBy = [ "graphical-session.target" ];
-    after = [ "swaylock.service" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.hyprland}/bin/Hyprland";
-    };
-  };
-
-  # Define systemd service for Waybar
-  systemd.user.services.waybar = {
-    description = "Waybar Status Bar";
-    wantedBy = [ "graphical-session.target" ];
-    after = [ "hyprland.service" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.waybar}/bin/waybar";
-    };
   };
 }
