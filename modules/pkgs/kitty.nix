4ea6decfe857a9efc20fd2c85c72b9586ba92263
Luis Victoria <v@lambda.lv>
2024-10-13 21:21:34 -0400

    Add background blur and documentation for Kitty config

--- modules/pkgs/kitty.nix
+++ modules/pkgs/kitty.nix
@@ -1,9 +1,12 @@
 { ... }:
+
 {
   programs.kitty = {
     enable = true;
 
+    # See: https://wiki.nixos.org/wiki/Kitty#Configuration
     settings = {
+      background_blur = 5;
       background_opacity = "0.65";
     };
   };
