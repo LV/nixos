19b3c637ba80de4769079f8d6ad07a0b2529aa8d
Luis Victoria <v@lambda.lv>
2024-10-13 21:16:43 -0400

    Remove kitty dotfiles from NixOS (not working for some reason)

--- modules/pkgs/kitty.nix
+++ modules/pkgs/kitty.nix
@@ -1,21 +1,10 @@
-{ config, inputs, pkgs, ... }:
-
-let
-  secrets = import ../../secrets.nix;
-  kittyConfigDir = "${config.home.homeDirectory}/.config/kitty";
-in
+{ ... }:
 {
   programs.kitty = {
     enable = true;
-  };
 
-  # Home Manager configuration for getting Kitty dotfiles
-  home.activation = {
-    cloneKittyDotfiles = inputs.home-manager.lib.hm.dag.entryBefore ["writeBoundary"] ''
-      # Clone the Hyprland config if it doesn't already exist
-      if [ ! -d ${kittyConfigDir} ]; then
-        ${pkgs.git}/bin/git clone https://${secrets.githubUsername}:${secrets.githubToken}@github.com/lv/kitty-config.git ${kittyConfigDir}
-      fi
+    extraConfig = ''
+      background_opacity 0.9
     '';
   };
 }
