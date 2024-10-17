6cd17275c4c50b51f311bbe44c30599f2ba2b493
Luis Victoria <v@lambda.lv>
2024-10-16 21:25:17 -0400

    Split home manager activation scripts

--- modules/pkgs/neovim.nix
+++ modules/pkgs/neovim.nix
@@ -23,16 +23,19 @@ in
 
   # Home Manager configuration for getting Neovim dotfiles
   home.activation = {
-    cloneNeovimDotfiles = inputs.home-manager.lib.hm.dag.entryBefore ["writeBoundary"] ''
+    cloneDotfilesNeovim = inputs.home-manager.lib.hm.dag.entryBefore ["writeBoundary"] ''
       # Clone the Neovim config if it doesn't already exist
       if [ ! -d ${nvimConfigDir} ]; then
         ${pkgs.git}/bin/git clone https://${secrets.githubUsername}:${secrets.githubToken}@github.com/lv/nvim-config.git ${nvimConfigDir}
       fi
+    '';
 
-      # Add alias for 'v' to run Neovim
+    addAliasNeovim = inputs.home-manager.lib.hm.dag.entryBefore ["writeBoundary"] ''
+      # Add alias 'v' to run Neovim
       if ! grep -q "alias v='nvim'" ~/.bashrc; then
         echo "alias v='nvim'" >> ~/.bashrc
       fi
+
       if ! grep -q "alias v='nvim'" ~/.zshrc; then
         echo "alias v='nvim'" >> ~/.zshrc
       fi
