bb791aeca6910a497c0325b40e09790ec2b95250
Luis Victoria <v@lambda.lv>
2024-10-19 18:06:04 -0400

    Use `nmtui` for network connections instead

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -4,13 +4,5 @@ let
   secrets = import ../../secrets.nix;
 in
 {
-  networking.wireless.enable = true;
-  networking.wireless.networks = {
-    "${secrets.wifiNetwork.ssid}" = {
-      psk = secrets.wifiNetwork.passkey;
-    };
-  };
-
-  # FALLBACK; uncomment if the above breaks; use `sudo nmtui`
-  # networking.networkmanager.enable = true;
+  networking.networkmanager.enable = true; # nmtui
 }
