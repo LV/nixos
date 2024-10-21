da4a14df3a45cbc5dc9c35d9a13c428969dd48f0
Luis Victoria <v@lambda.lv>
2024-10-20 22:07:59 -0400

    Add login manager and launch `Hyprland` when logged in

--- modules/system/display-setup.nix
+++ modules/system/display-setup.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 
+let
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+in
 {
   # Install swaylock and Hyprland
   environment.systemPackages = with pkgs; [
@@ -9,4 +12,32 @@
   programs.hyprland = {
     enable = true;
   };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${tuigreet} --time --remember --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
+  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+
+  #environment.etc."greetd/environments".text = ''
+  #  Hyprland
+  #  fish
+  #  bash
+  #'';
 }
