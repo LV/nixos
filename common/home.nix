c1931d1801a7d4801047d5d20d57575a4b42be5e
Luis Victoria <v@lambda.lv>
2024-11-16 14:48:28 -0500

    Add `gh` pkg

--- common/home.nix
+++ common/home.nix
@@ -14,6 +14,7 @@
     ../modules/home/pkgs/delta.nix
     ../modules/home/pkgs/emacs.nix
     ../modules/home/pkgs/flameshot.nix
+    ../modules/home/pkgs/github.nix
     ../modules/home/pkgs/go.nix
     ../modules/home/pkgs/hyprland.nix
     ../modules/home/pkgs/kitty.nix
