57e88f2c14b7856776d7da57e7ec9f98c5ade81c
Luis Victoria <v@lambda.lv>
2024-12-10 01:08:16 -0500

    Fix `elixir` export paths

--- modules/home/pkgs/elixir.nix
+++ modules/home/pkgs/elixir.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
@@ -7,4 +7,11 @@
     # Dependencies
     inotify-tools
   ];
+
+  home.activation = {
+    addNecessaryExports = inputs.home-manager.lib.hm.dag.entryBefore ["writeBoundary"] ''
+      export PATH=$HOME//nix/store/7s7zb9pdzk6h6s6mchvf9cbbsg3j8sbd-erlang-25.3.2.13/lib/erlang/erts-13.2.2.10/bin:$PATH
+      export PATH=$HOME//etc/profiles/per-user/lv/bin:$PATH
+    '';
+  };
 }
