3e16cd1b9a133c08f8fc2c6b161365647767361b
Luis Victoria <v@lambda.lv>
2025-01-01 03:13:01 -0500

    Use ghostty config

--- modules/home/pkgs/ghostty.nix
+++ modules/home/pkgs/ghostty.nix
@@ -1,3 +1,5 @@
+{ config, inputs, pkgs, ... }:
+
 let
   unstable = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/88195a94f390381c6afcdaa933c2f6ff93959cb4.tar.gz";
@@ -5,8 +7,25 @@ let
   }) {
     system = "x86_64-linux";
   };
+  ghosttyConfigDir = "${config.home.homeDirectory}/.config/ghostty";
 in {
   home.packages = [
     unstable.ghostty
   ];
+
+  home.activation = {
+    cloneDotfilesGhostty = inputs.home-manager.lib.hm.dag.entryBefore ["writeBoundary"] ''
+      # Clone the Ghostty config if it doesn't already exist
+      if [ ! -d ${ghosttyConfigDir} ]; then
+        # First try cloning with SSH
+        if ! GIT_SSH_COMMAND="${pkgs.openssh}/bin/ssh" ${pkgs.git}/bin/git clone git@github.com:lv/ghostty-config.git ${ghosttyConfigDir} 2>/dev/null; then
+          # Try HTTPS if SSH fails
+          if ! ${pkgs.git}/bin/git clone https://github.com/lv/ghostty-config.git ${ghosttyConfigDir}; then
+            echo "Failed to clone Ghostty config repository using both SSH and HTTPS"
+            exit 1
+          fi
+        fi
+      fi
+    '';
+  };
 }
